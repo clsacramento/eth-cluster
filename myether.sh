d0ee378aa3c5b0b556f07e09c50743757b66b353
Cynthia Lopes do Sacramento <cynthia.sacramento@hpe.com>
2017-02-02 22:01:25 +0000

    change datadir

--- myether.sh
+++ myether.sh
@@ -3,5 +3,10 @@ ETHPATH=$1
 NODE=$2
 geth init $ETHPATH/genesis.json
 sleep 1
-mkdir /ethdata
-geth --identity "$NODE" --rpc --rpcport "8000" --rpccorsdomain "*" --datadir "/ethdata" --port "30303" --nodiscover --ipcapi "admin,db,eth,debug,miner,net,shh,txpool,personal,web3" --rpcapi "db,eth,net,web3" --autodag --networkid 1900 --nat "any" 
+DIRECTORY=/ethdata
+if [ ! -d "$DIRECTORY" ]; then
+  # Control will enter here if $DIRECTORY doesn't exist.
+  mkdir $DIRECTORY
+fi
+cp $ETHPATH/genesis.json /ethdata/.
+geth --identity "$NODE" --rpc --rpcport "8000" --rpccorsdomain "*" --port "30303" --ipcapi "admin,db,eth,debug,miner,net,shh,txpool,personal,web3" --rpcapi "db,eth,net,web3" --autodag --networkid 1900 --nat "any" 
