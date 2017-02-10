7611aecfb49690ef06b4c3b73ffa566879e352cb
Cynthia Lopes do Sacramento <cynthia.sacramento@hpe.com>
2017-02-10 12:21:46 +0000

    Update myparity.sh

--- parity/myparity.sh
+++ parity/myparity.sh
@@ -2,14 +2,22 @@
 
 DIRECTORY=$1
 
+CONF_FILE=$2
+
 if [ ! -d "$DIRECTORY" ]; then
   # Control will enter here if $DIRECTORY doesn't exist.
   DIRECTORY=/parityconf
 fi
+
+if [ ! -d "$CONF_FILE" ]; then
+  # Control will enter here if $DIRECTORY doesn't exist.
+  CONF_FILE=node.toml
+fi
+
 
 cp -a /build/parity/target/release/parity /usr/bin/.
 
 
 cd /parityconf
 
-parity --config node.toml --jsonrpc-interface all --jsonrpc-hosts all --ui-interface $(hostname -i)
+parity --config $CONF_FILE --jsonrpc-interface all --jsonrpc-hosts all --ui-interface $(hostname -i)
